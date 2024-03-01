0bab266a12710a2380b94037ba5c0c858ddc978e
Luis Milanese <luis@vert-capital.com>
2024-03-01 01:05:54 -0300

    ação de marcar como concluído

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
+for (var i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
@@ -11,4 +10,21 @@ for (i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     });
+}
+
+var exercicios = document.getElementsByClassName("nome-exercicio");
+for (var i = 0; i < exercicios.length; i++) {
+    var concluido = document.createElement("div");
+    concluido.classList.add("concluido-button");
+    concluido.innerHTML = "Concluído";
+    concluido.addEventListener("click", function() {
+        marcarConcluido(this);
+    });
+
+    var exercicioAtual = exercicios[i];
+    exercicioAtual.after(concluido);
+}
+
+function marcarConcluido(el) {
+    el.parentNode.style.display = "none";
 }
